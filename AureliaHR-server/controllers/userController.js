680620d8acec34d4fc6a63abf6022f96af60ca2c
Jacek Kozłowski <jacek.kozlowski2000@gmail.com>
2025-01-09 03:15:46 +0100

    Version 0.6.1.0

--- AureliaHR-server/controllers/userController.js
+++ AureliaHR-server/controllers/userController.js
@@ -82,12 +82,12 @@ export const loginUser = async (req, res) => {
 
 export const logoutUser = async (req, res) => {
   try {
-    res.cookies("token", "", {
+    res.cookie("token", "", {
       httpOnly: true,
-      expires: new DataTransfer(0),
+      expires: new Date(0),
     })
 
-    res.status(200).jon({message: "Logout successful" })
+    res.status(200).json({message: "Logout successful" })
   } catch (error) {
     console.log(error)
     return res.status(400).json({ status: false, message: error.message})
@@ -112,9 +112,136 @@ export const getNotificationsList = async (req, res) => {
     const notice = await Notice.find({
       team: userId,
       isRead: { $nin: [userId] },
-    }).populate("task, title")
+    }).populate("task", "title")
 
     res.status(201).json(notice)
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json({ status: false, message: error.message })
+  }
+}
+
+export const updateUserProfile = async (req, res) => {
+  try {
+    const { userId, isAdmin } = req.user
+    const { _id } = req.body
+
+    const id = 
+      isAdmin && userId === _id
+      ? userId : isAdmin && userId !== _id
+      ? _id : userId
+
+    const user = await User.findById(id)
+
+    if (user) {
+      user.name = req.body.name || user.name
+      user.title = req.body.title || user.title
+      user.role = req.body.role || user.role
+    
+      const updatedUser = await user.save()
+
+      user.password = undefined
+
+      res.status(201).json({
+        status: true,
+        message: "Profile Updated Successfully",
+        user: updatedUser,
+      })
+    } else {
+      res.status(404).json({ status: false, message: "User not found" })
+    }
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json({ status: false, message: error.message})
+  }
+}
+
+export const markNotificationRead = async (req, res) => {
+  try {
+    const { userId } = req.user
+
+    const { isReadType, id } = req.query
+
+    if (isReadType === "all") {
+      await Notice.updateMany(
+        { team: userId, isRead: { $nin: [userId] }},
+        { $push: { isRead: userId }},
+        { new: true },
+      )
+    } else {
+      await Notice.findOneAndUpdate(
+        { _id: id, isRead: { $nin: [userId] }},
+        { $push: { isRead: userId }},
+        { new: true }
+      )
+    }
+
+    res.status(201).json({ status: true, message: "Done"})
+  } catch (error){
+    console.log(error)
+    return res.status(400).json({ status: false, message: error.message})
+  }
+}
+
+export const changeUserPassword = async (req, res) => {
+  try {
+    const { userId } = req.user
+
+    const user = await User.findById(userId)
+
+    if (user) {
+      user.password = req.body.password
+      
+      await user.save()
+
+      user.password = undefined
+
+      res.status(201).json({
+        status: true,
+        message: "Password changed successfully."
+      })
+    } else {
+      res.status(404).json({ status: false, message: "User not found"})
+    }
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json({ status: false, message: error.message })
+  }
+}
+
+export const activateUserProfile = async (req, res) => {
+  try{
+    const { id } = req.params
+
+    const user = await User.findById(id)
+
+    if (user) {
+      user.isActive = req.body.isActive
+      
+      await user.save()
+
+      res.status(201).json({
+        status: true,
+        message: `User account has been ${user?.isActive ? "activated" : "disabled"}`
+      })
+    } else {
+      res.status(404).json({ status: false, message: "User not found"})
+    }
+  } catch (error) {
+    console.log(error)
+    return res.status(400).json({ status: false, message: error.message })
+  }
+}
+
+export const deleteUserProfile = async (req, res) => {
+  try{
+    const { id } = req.params
+
+    await User.findByIdAndDelete(id)
+
+    res
+      .status(200)
+      .json({ status: true, message: "User deleted successfully" })
   } catch (error) {
     console.log(error)
     return res.status(400).json({ status: false, message: error.message })
